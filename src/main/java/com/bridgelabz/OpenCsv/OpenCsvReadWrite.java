89a500b76b122846afccd2d9cea2b1a4d56e2b2d
pooja todkar <poojatodkar124@gmail.com>
2019-11-28 17:54:25 +0530

    Generate CSV file from List of Objects

--- src/main/java/com/bridgelabz/OpenCsv/OpenCsvReadWrite.java
+++ src/main/java/com/bridgelabz/OpenCsv/OpenCsvReadWrite.java
@@ -2,40 +2,39 @@ package com.bridgelabz.OpenCsv;
 
 
 import com.opencsv.CSVWriter;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 
-import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class OpenCsvReadWrite {
 
-    private static final String SAMPLE_CSV_FILE_PATH = "/home/abc/Desktop/string-array-sample.csv";
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/abc/Desktop/object-list-sample.csv";
 
-    public static void main(String[] args) throws IOException {
-    try(
-            Writer writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH));
-            CSVWriter csvWriter = new CSVWriter(writer,
-                    CSVWriter.DEFAULT_SEPARATOR,
-                    CSVWriter.DEFAULT_QUOTE_CHARACTER,
-                    CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                    CSVWriter.DEFAULT_LINE_END);
-            ){
-        String[] headerRecord = {"Name","Email","Phone","Country"};
-        csvWriter.writeNext(headerRecord);
+    public static void main(String[] args) throws IOException,
+            CsvDataTypeMismatchException,
+            CsvRequiredFieldEmptyException {
 
-        csvWriter.writeNext(new String[]{"Pooja Todkar","poojatodkar124@gmail.com","7304278325","India"});
-        csvWriter.writeNext(new String[]{"Pooja Todkar1","poojatodkar1124@gmail.com","73042783215","India1"});
+        try (
+                Writer writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH));
+        ) {
+            StatefulBeanToCsv<MyUser> beanToCsv = new StatefulBeanToCsvBuilder(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .build();
 
-    }
+            List<MyUser> myUsers = new ArrayList<>();
+            myUsers.add(new MyUser("Sundar Pichai ♥", "sundar.pichai@gmail.com", "+1-1111111111", "India"));
+            myUsers.add(new MyUser("Satya Nadella", "satya.nadella@outlook.com", "+1-1111111112", "India"));
+
+            beanToCsv.write(myUsers);
+        }
     }
 }
